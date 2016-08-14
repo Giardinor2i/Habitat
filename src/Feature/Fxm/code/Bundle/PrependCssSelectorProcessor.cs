5d9e2fec70d589b7af562605100ac2958f388856
v-tibar <timothy.barreto@gmail.com>
2016-08-13 20:16:25 -0700

    Add prepend css selector bundle processor that provides isolation for styles used in the context of FXM

---
+++ src/Feature/Fxm/code/Bundle/PrependCssSelectorProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Diagnostics;
+using System.Web.Optimization;
+using Sitecore.FXM.Pipelines.Bundle;
+
+namespace Sitecore.Feature.Fxm.Bundle
+{
+    public class PrependCssSelectorProcessor : IBundleProcessor, IBundleProcessor<IBundleGeneratorArgs>
+    {
+        public void Process(IBundleGeneratorArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, "args");
+            var transform = new PrependCssSelectorTransform();
+            args.Bundle.Transforms.Add(transform);
+        }
+    }
+}
---
+++ src/Feature/Fxm/code/Bundle/PrependCssSelectorTransform.cs
@@ -0,0 +1,116 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Configuration;
+using ExCSS;
+using System;
+using System.Text;
+using System.Web.Optimization;
+
+namespace Sitecore.Feature.Fxm.Bundle
+{
+    public class PrependCssSelectorTransform : IBundleTransform
+    {
+        private string _selector;
+
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            _selector = Settings.GetSetting("Bundle.PrependCssSelectorProcessor.Selector");
+
+            try
+            {
+                StyleSheet styleSheet = new Parser().Parse(response.Content);
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (RuleSet sheetRule in styleSheet.Rules)
+                {
+                    try
+                    {
+                        stringBuilder.AppendLine(this.FormatRuleSet(sheetRule));
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Exception occured", ex, (object)this);
+                    }
+                }
+                response.Content = stringBuilder.ToString();
+                response.ContentType = "text/css";
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Exception occured in FxmTransform method", ex, this);
+                throw;
+            }
+        }
+
+        private string FormatRuleSet(RuleSet sheetRule)
+        {
+            string str1 = string.Empty;
+            if (!(sheetRule is AggregateRule))
+                return this.FormatRule(sheetRule);
+
+            foreach (RuleSet rule in (sheetRule as AggregateRule).RuleSets)
+            {
+                string str2 = this.FormatRule(rule);
+                if (str1 != string.Empty)
+                    str1 += Environment.NewLine;
+                str1 += str2;
+            }
+
+            if (!(sheetRule is MediaRule))
+                return str1;
+
+            return "@media " + (sheetRule as MediaRule).Media.MediaType + "{" + Environment.NewLine + str1 + Environment.NewLine + "}";
+        }
+
+        private string FormatRule(RuleSet rule)
+        {
+            string str2;
+            if (rule is StyleRule)
+            {
+                StyleRule styleRule = rule as StyleRule;
+                str2 = this.ModifySelector(styleRule.Selector.ToString()) + " {" + Environment.NewLine + this.FormatDeclarations(styleRule.Declarations.ToString()) + "}";
+            }
+            else
+                str2 = rule.ToString();
+
+            return str2;
+        }
+
+        private string FormatDeclarations(string declarations)
+        {
+            string str1 = string.Empty;
+            string str2 = declarations.Trim();
+            foreach (string str3 in str2.Split(';'))
+            {
+                if (!string.IsNullOrEmpty(str3.Trim()))
+                    str1 = str1 + str3 + ";" + Environment.NewLine;
+            }
+            return str1;
+        }
+
+        private string ModifySelector(string selector)
+        {
+            string[] strArray = selector.Split(',');
+            StringBuilder stringBuilder = new StringBuilder();
+            int index = 0;
+            while (index < strArray.Length)
+            {
+                string item = strArray[index];
+
+                if (!item.Trim().Equals(_selector, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    stringBuilder.Append(_selector + " " + item);
+                    if (index < checked(strArray.Length - 1))
+                        stringBuilder.Append("," + Environment.NewLine);
+                }
+                else
+                {
+                    stringBuilder.Append(item);
+                    if (index < checked(strArray.Length - 1))
+                        stringBuilder.Append("," + Environment.NewLine);
+                }
+
+                checked { ++index; }
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
